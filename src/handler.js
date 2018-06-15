c8cbf111b3361add245c63c9b63e2619d9391338
Ben McPhail <ben@presentcompany.co>
2018-06-15 15:22:41 +1000

    fun test

--- src/handler.js
+++ src/handler.js
@@ -1,80 +1,68 @@
 import * as GoogleImages from 'google-images';
+import * as randomItemInArray from 'random-item-in-array';
 
 if (!global._babelPolyfill) {
-   require('babel-polyfill');
+	require('babel-polyfill');
 }
 
 exports.header = async (event, context, cb) => {
-  const html = `
-    <html>
-      <body>
-        <h1>
-          header
-        </h1>
-      </body>
-    </html>
-  `;
+	const html = `
+		<html>
+			<body>
+				<h1>
+					header
+				</h1>
+			</body>
+		</html>
+	`;
 
-  const response = {
-    statusCode: 200,
-    headers: {
-      'Content-Type': 'text/html',
-    },
-    body: html,
-  };
+	const response = {
+		statusCode: 200,
+		headers: {
+			'Content-Type': 'text/html',
+		},
+		body: html,
+	};
 
-  cb(null, response);
+	cb(null, response);
 }
 
 export const normal = (event, context, cb) => {
-  cb(null, 'normal response');
+	cb(null, 'normal response');
 }
 
 export const promise = (event, context, cb) => {
-  const p = new Promise((resolve, reject) => {
-    resolve('promise success');
-  });
-  p
-    .then(r => cb(null, r))
-    .catch(e => cb(e));
+	const p = new Promise((resolve, reject) => {
+		resolve('promise success');
+	});
+	p
+		.then(r => cb(null, r))
+		.catch(e => cb(e));
 }
 
 export const bongBot = (event, context, cb) => {
-  const title = event.query.text;
+	const title = `bong ${event.query.text}`;
 
-  const image = "http://static1.squarespace.com/static/55ccf522e4b0fc9c2b651a5d/t/55cd0f80e4b01a54c907078f/1439502211367/Slayer_Social_Sharing_Logo.jpg?format=1000w";
+	var client = new GoogleImages(
+		process.env.google_cse_id,
+		process.env.google_cse_api_key
+	);
+	
+	client.search(title, {
+		safeSearch: 'high',
+		type: 'photo'
+	}).then(images => {
 
-  cb(null, {
-    "text": "Brrruuuhhh",
-    "attachments": [
-        {
-            "image_url": image,
-        }
-    ]
-  });
-  // cb(null, {
-  //     "text": "http://static1.squarespace.com/static/55ccf522e4b0fc9c2b651a5d/t/55cd0f80e4b01a54c907078f/1439502211367/Slayer_Social_Sharing_Logo.jpg?format=1000w"
-  // });
-  // return 'hi';
+		var image = randomItemInArray(images);
 
-  // console.log(process.env.VAR_1);
-  // var client = new GoogleImages(process.env.google_cse_id, process.env.google_cse_api_key);
-  
-  // var query = randomItemInArray(['bong','bongs']) + " " + req.body.text;
-
-  // client.search(query, {
-  //   safeSearch: 'high',
-  //   type: 'photo'
-  // }).then(images => {
-  //   var image = randomItemInArray(images);
-  //   if(image) {
-  //     res.json({
-  //       "response_type": "in_channel",
-  //       "attachments":[{
-  //         "image_url": image.url
-  //       }]
-  //     });
-  //   }
-  // });
+		cb(null, {
+			"text": "Brrruuuhhh",
+			"attachments": [
+					{
+							"image_url": image.url,
+					}
+			]
+		});
+	});
 };
 
